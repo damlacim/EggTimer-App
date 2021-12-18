2f04020ed3fb0414144b2811b9a74e6401c2a006
damlacim <cim.damla@gmail.com>
2021-12-19 01:06:26 +0300

    add countdown timer code

--- EggTimer App/ViewController.swift
+++ EggTimer App/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var secondsRemaining = 60
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.titleLabel?.text ?? "Soft"
-        print(eggTimes[hardness])
+        secondsRemaining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        }
     }
     
 }
