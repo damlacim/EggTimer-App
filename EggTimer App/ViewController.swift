3b3e0b2c9ae60b22d84cef1a4df95ca374c06752
damlacim <cim.damla@gmail.com>
2021-12-19 00:49:23 +0300

    add dict

--- EggTimer App/ViewController.swift
+++ EggTimer App/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.titleLabel?.text ?? "Soft"
+        print(eggTimes[hardness])
     }
-
-
+    
 }
 
